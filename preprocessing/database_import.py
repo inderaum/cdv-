d66d3b11de81e30766065a246383b6f1a0ba25f2
inderaum <inderaum@gmail.com>
2019-04-10 14:45:28 +0200

    fix: csv import datatypes

--- preprocessing/database_import.py
+++ preprocessing/database_import.py
@@ -1,4 +1,3 @@
-
 import pandas as pd
 import codecs
 
@@ -7,10 +6,35 @@ import codecs
 
 def escape_special_characters():
     with codecs.open('../data/raw_data.csv', 'r', encoding='latin1', errors='ignore') as csv:
-        raw_data = pd.read_csv(csv, sep=';', error_bad_lines=True)
+        raw_data = pd.read_csv(csv, sep=';', error_bad_lines=True,
+                               dtype={"AUS": object,
+                                      "LP_NR": int,
+                                      "SEITE": object,
+                                      "TITEL_NR": int,
+                                      "ART": str,
+                                      "LPTITEL": str,
+                                      "TITEL": str,
+                                      "BAND": str,
+                                      "DIRIGENT": str,
+                                      "KOMPONIST": str,
+                                      "INTERPR1": str,
+                                      "INTERPR2": str,
+                                      "INTERPR3": str,
+                                      "INTERPR4": str,
+                                      "INTERPR5": str,
+                                      "INTERPR6": str,
+                                      "INTERPR7": str,
+                                      "INTERPR8": str,
+                                      "INTERPR9": str,
+                                      "INTERPR10": str,
+                                      "ANMERKUNG": str,
+                                      "MUSIKART": str,
+                                      "JAHR": int,
+                                      "MIN": int,
+                                      "NOTE": object,
+                                      "SEK": int,
+                                      "WAHL": int})
         for column in raw_data:
-            print(column)
-            if pd.isna(column[1]):
-                print("NaN found")
+            print(raw_data[column])
 
 escape_special_characters()
